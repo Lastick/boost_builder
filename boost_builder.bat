6b739004210d187b796c664d5c50d6eed0abf6ff
Vasya Ichkoff <vasyaichkoff@gmail.com>
2021-01-27 14:59:48 +0200

    Revert 1.65.1 (Windows)

--- boost_builder.bat
+++ boost_builder.bat
@@ -1,20 +1,21 @@
 
-set INSTALL_DIR="D:\boost"
+set INSTALL_DIR="D:\dev\boost"
 
 set PATH=%PATH%;bin
 
-curl.exe -L https://sourceforge.net/projects/boost/files/boost/1.73.0/boost_1_73_0.tar.gz/download > boost_1_73_0.tar.gz
-tar.exe -xvzf boost_1_73_0.tar.gz
+curl.exe -L https://sourceforge.net/projects/boost/files/boost/1.65.1/boost_1_65_1.tar.gz/download > boost_1_65_1.tar.gz
+tar.exe -xvzf boost_1_65_1.tar.gz
 
-rm.exe -f boost_1_73_0.tar.gz
+rm.exe -f boost_1_65_1.tar.gz
 
-cd boost_1_73_0
+cd boost_1_65_1
 mkdir.exe -p %INSTALL_DIR%
 
 call bootstrap.bat
 b2.exe link=static ^
        runtime-link=static ^
-       toolset=msvc ^
+       toolset=msvc-14.0 ^
+       address-model=64 ^
        architecture=x86 ^
        --with-chrono ^
        --with-date_time ^
@@ -32,4 +33,4 @@ b2.exe link=static ^
        --prefix=%INSTALL_DIR% install
 
 cd ..
-rm.exe -rf boost_1_73_0
+rm.exe -rf boost_1_65_1
